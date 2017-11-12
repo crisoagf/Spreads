bb431b6ad317791854811e86914eb96f5feaeba3
Cristóvão Gomes Ferreira <crisoagf@gmail.com>
2017-11-12 17:19:30 +0000

    Primeira versão com ncurses

--- src/Spread.hs
+++ src/Spread.hs
@@ -1,29 +1,58 @@
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RankNTypes #-}
 module Spread where
-import Data.Array
-import Data.Map (Map)
-import qualified Data.Map as M
-import Numeric.Natural
 import Spread.StdLib.Int
 import Spread.StdLib.Range
 import Spread.TypesAndVals
 import Spread.Typecheck
 import Spread.Parser
+
 import Control.Arrow hiding (first, second)
+import Control.Lens
 import Control.Monad
+import Control.Monad.IO.Class
 import Control.Monad.Trans.Except
 import Control.Monad.Trans.State
 import Control.Monad.Trans.Reader
+
+import Data.Array
 import Data.Bifunctor
-import Morte.Core
+import Data.Function
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Monoid ((<>))
+
+import Data.Text.Buildable
 import Data.Text.Lazy (Text)
 import qualified Data.Text.Lazy as T
 import qualified Data.Text.Lazy.IO as TIO
+
+import System.Console.Haskeline
+
+import Numeric.Natural
+import UI.NCurses
+import Morte.Core
 import Text.Parsec
 
-data Visions = Input | Value | Presentation
+instance MonadException Curses where
+  controlIO act = join $ liftIO $ act $ RunIO pure
+
+data Vision = Input | Type | Value | Presentation
+data Mode = Normal | Edit CellIndex
+data EditorState = EditorState { _currentSheet :: Array CellIndex Text, _currentCell :: CellIndex, _selColor :: ColorID, _norColor :: ColorID, _windowHeight :: Integer, _windowWidth :: Integer, _cellConfig :: CellConfig, _currentVision :: Vision, _currentMode :: Mode, _statusBar :: Text }
+data CellConfig = CellConfig { _cellHeight :: Integer, _cellWidth :: Integer, _numVisRows :: Integer, _numVisCols :: Integer} deriving Eq
+
+joinLenses :: Lens s s a a -> Lens s s b b -> Lens s s (a, b) (a, b)
+joinLenses la lb = lens (\ s -> (view la s, view lb s)) (\ s (a,b) -> (s & la .~ a) & lb .~ b)
+
+makeLenses ''EditorState
+makeLenses ''CellConfig
+
+spreadsheetParse :: Array CellIndex Text -> Array CellIndex (Either ParseError CellExpr)
+spreadsheetParse = fmap (parse parseCell "input")
 
 spreadsheetInferTypes :: (NamedReference -> Maybe CellRawExpr) -> Array CellIndex CellExpr -> Array CellIndex (Either TypeError CellType)
 spreadsheetInferTypes refMap arr = fmap (join . fmap ((`runReader` (InferContext (either (fmap snd . getType) (ExceptT . pure . fmap snd . (go !))) refMap)) . runExceptT . uncurry returnTypes)) $ go where go = fmap ((`runReader` (InferContext (either (fmap snd . getType) (ExceptT . pure . fmap snd . (go !))) refMap)) . runExceptT . getType) arr
@@ -31,7 +60,7 @@ spreadsheetInferTypes refMap arr = fmap (join . fmap ((`runReader` (InferContext
 spreadsheetEval :: (NamedReference -> Maybe CellRawExpr) -> Array CellIndex CellExpr -> Array CellIndex (Either TypeError CellWithType)
 spreadsheetEval refMap arr = go where go = fmap ((`runReader` (EvalContext (either typeAndEvaluate (ExceptT . pure . (go !))) refMap)) . runExceptT . typeAndEvaluate) arr
 
-exampleSheet :: Array (Natural, Natural) String
+exampleSheet :: Array (Natural, Natural) Text
 exampleSheet = listArray ((0,0),(2,4))
   [ "1", "2", "3.5", "#Int", "[6,7,8:Int]"
   , "4", "5", "@{1,2}", "Plus 8 5", "\"1\""
@@ -49,19 +78,77 @@ exampleSheetEval = either (Left . Left) (first Right) $ (sequence . spreadsheetE
 stdLib :: NamedReference -> Maybe CellRawExpr
 stdLib = flip lookup (basicIntFns ++ basicRangeFns)
 
-example :: Array (Natural, Natural) CellExpr
-example = listArray ((0,0),(2,2))
-  [Embed $ RawValue $ IntValue 1, Embed $ RawValue $ IntValue 2, App (Embed (NamedRefValue "Plus")) (Embed (RawValue $ IntValue 5)),
-   Embed $ RawValue $ IntValue 4, Embed $ RawValue $ IntValue 5, Embed $ RawValue $ PosValue (1,2),
-   App (Embed $ RefValue (0,2)) (Embed $ RawValue $ IntValue 3), App (App (App (Embed (NamedRefValue "Range")) (Embed $ RawValue $ TypeValue (Embed Int))) (Embed (RawValue $ PosValue (0,0)))) (Embed $ RawValue $ PosValue (1,1)), App (App (App (Embed (NamedRefValue "Range")) (Embed $ RawValue $ TypeValue (Embed Float))) (Embed (RawValue $ PosValue (0,0)))) (Embed $ RawValue $ PosValue (1,1))]
-
-exampleTypes = spreadsheetInferTypes stdLib example
-exampleEval = spreadsheetEval stdLib example
-
-showExample = mapM_ (TIO.putStrLn . pretty) example
-showExampleTypes = mapM_ (either (TIO.putStrLn . pretty) (TIO.putStrLn . pretty)) exampleTypes
-showExampleEval = mapM_ (either (TIO.putStrLn . pretty) (TIO.putStrLn . pretty)) exampleEval
-
 main :: IO ()
-main = putStrLn "Under construction!"
+main = do
+  runCurses $ do
+    win <- defaultWindow
+    (h, w) <- updateWindow win windowSize
+    normalColor <- newColorID ColorWhite ColorDefault 1
+    selectedColor <- newColorID ColorBlack ColorWhite 2
+    let defaultCellConfig = CellConfig 1 (w `div` 15) (min h ((1+) $ fromIntegral $ uncurry (flip (-) `on` fst) $ bounds exampleSheet)) (fromIntegral $ min 15 ((1+) $ uncurry (flip (-) `on` snd) $ bounds exampleSheet))
+    let defaultEdState = EditorState exampleSheet (0,0) selectedColor normalColor h w defaultCellConfig Input Normal (exampleSheet ! (0,0))
+    setEditorState win defaultEdState 
+    fix (\ loop edState -> do
+      ev <- getEvent win Nothing
+      let (newEdState', exit) = modeInterpreter edState (_currentMode edState) ev
+      when (not exit) $ do
+        when (view cellConfig edState /= view cellConfig newEdState') (updateWindow win clear)
+        let newEdState = newEdState' { _statusBar = (uncurry (!) (newEdState' ^. joinLenses currentSheet currentCell))}
+        setEditorState win newEdState
+        loop newEdState) defaultEdState
+
+setEditorState :: Window -> EditorState -> Curses ()
+setEditorState win edState = updateWindow win (showSheet edState >> updateStatusBar edState) >> render
+
+modeInterpreter :: EditorState -> Mode -> Maybe Event -> (EditorState, Bool)
+modeInterpreter edState Normal ev =
+  let w = _windowWidth edState in case ev of
+        Just (EventCharacter 'q') -> (edState, True)
+        Just (EventCharacter 'i') -> (edState & currentVision .~ Input, False)
+        Just (EventCharacter 't') -> (edState & currentVision .~ Type , False)
+        Just (EventCharacter 'v') -> (edState & currentVision .~ Value, False)
+        Just (EventCharacter '+') -> (edState & joinLenses (cellConfig . numVisCols) (cellConfig . cellWidth) %~ (\ (visCols, cw) -> (visCols, min (w `div` visCols) (cw + 1))), False)
+        Just (EventCharacter '-') -> (edState & (cellConfig . cellWidth) %~ (\ cw -> max 1 (cw - 1)), False)
+        Just (EventSpecialKey KeyLeftArrow)  -> (edState & joinLenses currentSheet currentCell %~ (\ (exampleSheet, (r,c)) -> (exampleSheet, (r, max c (snd (fst $ bounds exampleSheet) + 1) - 1))), False)
+        Just (EventSpecialKey KeyRightArrow) -> (edState & joinLenses currentSheet currentCell %~ (\ (exampleSheet, (r,c)) -> (exampleSheet, (r, min c (snd (snd $ bounds exampleSheet) - 1) + 1))), False)
+        Just (EventSpecialKey KeyUpArrow)    -> (edState & joinLenses currentSheet currentCell %~ (\ (exampleSheet, (r,c)) -> (exampleSheet, (max r (fst (fst $ bounds exampleSheet) + 1) - 1, c))), False)
+        Just (EventSpecialKey KeyDownArrow)  -> (edState & joinLenses currentSheet currentCell %~ (\ (exampleSheet, (r,c)) -> (exampleSheet, (min r (fst (snd $ bounds exampleSheet) - 1) + 1, c))), False)
+        _ -> (edState, False)
+
+showSheet :: EditorState -> Update ()
+showSheet edState =
+  let s      = _currentSheet edState
+      selInd = _currentCell edState
+      sel    = _selColor edState
+      nor    = _norColor edState
+      h      = _windowHeight edState
+      w      = _windowWidth edState
+      ch     = min (_cellHeight (_cellConfig edState)) (h `div` _numVisRows (_cellConfig edState))
+      cw     = min (_cellWidth  (_cellConfig edState)) ((w - 1) `div` _numVisCols (_cellConfig edState)) in case _currentVision edState of
+        Input -> forM_ (assocs $ fmap (parse parseCell "input") s) (showCell ch cw selInd nor sel)
+        Type -> case sequence (spreadsheetParse s) of
+          Left pe -> moveCursor h 0 >> drawText "Please solve all parsing errors before typing"
+          Right sheet -> forM_ (assocs $ spreadsheetInferTypes stdLib sheet) (showCell ch cw selInd nor sel)
+        Value -> case sequence (spreadsheetParse s) of
+          Left pe -> moveCursor h 0 >> drawText "Please solve all parsing errors before evaluating"
+          Right sheet -> forM_ (assocs $ fmap (fmap cellValue) $ spreadsheetEval stdLib sheet) (showCell ch cw selInd nor sel)
+
+showCell :: (Show e, Buildable s) => Integer -> Integer -> CellIndex -> ColorID -> ColorID -> (CellIndex, Either e s) -> Update ()
+showCell ch cw (sx, sy) nor sel ((i,j), t) = do
+  moveCursor (fromIntegral i * ch) (fromIntegral j * cw)
+  drawString (replicate (fromIntegral $ cw - 1) ' ')
+  moveCursor (fromIntegral i * ch) (fromIntegral j * cw)
+  when (sx == i && sy == j) $ setColor sel
+  let t' = padAndTruncate (cw - 1) $ either (T.pack . show) pretty t
+  drawText (T.toStrict t')
+  when (sx == i && sy == j) $ setColor nor
+
+updateStatusBar :: EditorState -> Update ()
+updateStatusBar edState = do
+  moveCursor (_windowHeight edState - 1) 0
+  clearLine
+  drawText (T.toStrict $ _statusBar edState)
+
+padAndTruncate :: Integral i => i -> Text -> Text
+padAndTruncate i t = if T.length t <= fromIntegral i then t <> T.replicate (fromIntegral i - T.length t) " " else T.take (fromIntegral i - 3) t <> "..."
 
--- src/Spread/Parser.hs
+++ src/Spread/Parser.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings, FlexibleContexts #-}
 module Spread.Parser where
 import Spread.TypesAndVals
+import Data.Text.Lazy (Text)
 import qualified Data.Text.Lazy as T
 import Text.Parsec
 import Text.Parsec.Expr
@@ -10,16 +11,16 @@ import Data.Ix
 import Data.Bifunctor
 import Morte.Core
 
-parseCell :: Stream String m t => ParsecT String u m CellExpr
+parseCell :: Stream Text m t => ParsecT Text u m CellExpr
 parseCell = parseApps <* eof
 
-parseApps :: Stream String m t => ParsecT String u m CellExpr
+parseApps :: Stream Text m t => ParsecT Text u m CellExpr
 parseApps = between (string "(") (string ")") parseApps <|> (findApps =<< expr `sepBy1` string " ")
  where findApps [] = fail "cell expression"
        findApps [a] = pure a
        findApps (a:b:as) = pure $ foldl' App (App a b) as
 
-expr :: Stream String m t => ParsecT String u m CellExpr
+expr :: Stream Text m t => ParsecT Text u m CellExpr
 expr =
   try (Embed . RawValue <$> rawValue) <|>
   lambda <|>
@@ -28,19 +29,19 @@ expr =
   namedRef <|>
   between (string "(") (string ")") parseApps <?> "cell expression"
 
-rawValue :: Stream String m t => ParsecT String u m CellRawValue
+rawValue :: Stream Text m t => ParsecT Text u m CellRawValue
 rawValue =
   try (FloatValue <$> fractional) <|>
   try (IntValue <$> int) <|>
-  ((StringValue . T.pack) <$> between (string "\"") (string "\"") (many escapedString)) <|>
+  ((StringValue . T.pack) <$> between (string "\"") (string "\"") (many escapedText)) <|>
   (flip ListValue <$> between (string "[") (string ":") ((fmap Embed rawValue) `sepBy` string ",") <*> (typeValue <* string "]")) <|>
   (PosValue <$> (string "@" *> between (string "{") (string "}") ((,) <$> (fromIntegral <$> nat) <* string "," <*> (fromIntegral <$> nat)))) <|>
   (TypeValue <$> (string "#" *> typeValue)) <?> "a raw cell value"
 
-escapedString :: Stream String m t => ParsecT String u m Char
-escapedString = (noneOf "\\\"" <|> (char '\\' *> oneOf "\\\""))
+escapedText :: Stream Text m t => ParsecT Text u m Char
+escapedText = (noneOf "\\\"" <|> (char '\\' *> oneOf "\\\""))
 
-typeValue :: Stream String m t => ParsecT String u m CellType
+typeValue :: Stream Text m t => ParsecT Text u m CellType
 typeValue =
   between (string "(") (string ")") (buildExpressionParser [[Infix (try (string " -> ") *> pure (Pi "")) AssocRight], [Infix (many1 (char ' ') *> pure App) AssocLeft]] ((Embed <$> basicType) <|>
     ((Var . flip V 0  . T.pack) <$> ((:) <$> lower <*> many alphaNum))) <|>
@@ -50,7 +51,7 @@ typeValue =
   (string "*" >> pure (Const Star)) <|>
   between (string "(") (string ")") typeValue <?> "a type"
 
-basicType :: Stream String m t => ParsecT String u m RawType
+basicType :: Stream Text m t => ParsecT Text u m RawType
 basicType = 
   (string "Int"    *> pure    Int) <|>
   (string "Float"  *> pure  Float) <|>
@@ -60,16 +61,16 @@ basicType =
   (string "Pos"    *> pure    Pos) <|>
   (string "List"   *> pure   List) <?> "basic type"
 
-variable :: Stream String m t => ParsecT String u m CellExpr
+variable :: Stream Text m t => ParsecT Text u m CellExpr
 variable = (Var . flip V 0 . T.pack) <$> ((:) <$> lower <*> many alphaNum) <?> "variable"
 
-namedRef :: Stream String m t => ParsecT String u m CellExpr
+namedRef :: Stream Text m t => ParsecT Text u m CellExpr
 namedRef = (Embed . NamedRefValue . T.pack) <$> ((:) <$> upper <*> many alphaNum) <?> "named reference"
 
-ref :: Stream String m t => ParsecT String u m CellExpr
+ref :: Stream Text m t => ParsecT Text u m CellExpr
 ref = (Embed . RefValue) <$> (string "@!" *> between (string "{") (string "}") ((,) <$> (fromIntegral <$> nat) <* string "," <*> (fromIntegral <$> nat)))
 
-lambda :: Stream String m t => ParsecT String u m CellExpr
+lambda :: Stream Text m t => ParsecT Text u m CellExpr
 lambda = do
   string "\\"
   var <- fmap T.pack ((:) <$> lower <*> many alphaNum)
